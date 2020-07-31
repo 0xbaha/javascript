9f230f3a7183a7ff0eb7386c71f5598709d868c9
Baha ID <dev@baha.web.id>
2020-08-01 01:09:16 +0700

    Update displayObject.js

--- sample/displayObject.js
+++ sample/displayObject.js
@@ -1,6 +1,8 @@
 // this sample implemented in Discord command
 
+var authorAvatarURL = msg.author.displayAvatarURL();
 var sum = msg.mentions.users.size;  // sum of mentioned users
+var isMentioned = false;
 var Mentioned = msg.mentions.users; // the mentioned users
 
 // takes a JavaScript object and then transforms it into a JSON string.
@@ -8,24 +10,48 @@ var MentionedSTR = JSON.stringify(Mentioned);
 // beautify 
 var MentionedSTRbeauty = JSON.stringify(Mentioned, null, 4);
 
+// print author avatar
+console.log("\n>>> authorAvatarURL = " + authorAvatarURL);
+
 // print all to console
 if (sum > 0) {
-    console.log("ADA mention");
-    console.log("\n\n>>> sum = " + sum);
-    console.log("\n\n>>> msg.mentions.users <<<\n\n" + Mentioned);
-    console.log("\n\n>>> JSON.stringify(Mentioned) <<<\n\n" + MentionedSTR);  // print the object
-    console.log("\n\n>>> JSON.stringify(Mentioned, null, 4) <<<\n\n" + MentionedSTRbeauty);  // beautify
+  isMentioned = true;
+  console.log("\nADA mention");
+  console.log("\n\n>>> sum = " + sum);
+  console.log("\n\n>>> msg.mentions.users <<<\n\n" + Mentioned);
+  console.log("\n\n>>> JSON.stringify(Mentioned) <<<\n\n" + MentionedSTR);  // print the object
+  console.log("\n\n>>> JSON.stringify(Mentioned, null, 4) <<<\n\n" + MentionedSTRbeauty);  // beautify
 } else {
-    console.log("TIDAK ada mention");
+  isMentioned = false;
+  console.log("\nTIDAK ada mention");
 }
 
 // print the ID of each mentioned user
-console.log("\n\n>>> print the ID of each mentioned user <<<\n");
-for (i = 0; i < sum; i++) {
-    // takes a JSON string and then transforms it into a JavaScript object
+avatarURLs = []; // inisialisasi
+console.log("\n>>> print the ID of each mentioned user <<<\n");
+if (isMentioned){
+  for (i = 0; i < sum; i++) {
     console.log("ID = " + JSON.parse(MentionedSTR)[i].id);
+    // menambahkan URL avatar format PNG ke array
+    avatarURLs.push(JSON.parse(MentionedSTR)[i].displayAvatarURL.replace(/webp/gi,"png?size=2048"));
+  }
+} else {
+  avatarURLs.push(authorAvatarURL.replace(/webp/gi,"png?size=2048"));
 }
 
-// reference:
+// menampilkan avatar setiap yang dimention
+if (sum === 0) {
+  for (i = 0; i <= sum; i++) {
+    console.log("\navatarURLs["+ i +"] = " + avatarURLs[i]);
+    msg.channel.send(avatarURLs[i]);
+  }
+} else {
+  for (i = 0; i < sum; i++) {
+    console.log("\navatarURLs["+ i +"] = " + avatarURLs[i]);
+    msg.channel.send(avatarURLs[i]);
+  }
+}
+
+// References:
 // https://stackoverflow.com/questions/41336663/console-logresult-returns-object-object-how-do-i-get-result-name
 // https://medium.com/javascript-in-plain-english/how-to-use-stringify-and-parse-in-javascript-6b637b571a32
