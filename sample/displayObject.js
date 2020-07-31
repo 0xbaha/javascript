c3afd0f050946249bd097713f99db3da6137201f
Baha ID <dev@baha.web.id>
2020-07-31 22:53:25 +0700

    Update displayObject.js

--- sample/displayObject.js
+++ sample/displayObject.js
@@ -1,12 +1,28 @@
 // this sample implemented in Discord command
 
-if (msg.mentions.users.size) {
+var sum = msg.mentions.users.size;  // sum of mentioned users
+var Mentioned = msg.mentions.users; // the mentioned users
+
+// takes a JavaScript object and then transforms it into a JSON string.
+var MentionedSTR = JSON.stringify(Mentioned);   
+// beautify 
+var MentionedSTRbeauty = JSON.stringify(Mentioned, null, 4);
+
+// print all to console
+if (sum > 0) {
     console.log("ADA mention");
-    console.log("msg.mentions.users = " + msg.mentions.users);
-    console.log("msg.mentions.users = " + JSON.stringify(msg.mentions.users));  // print the object
-    console.log("msg.mentions.users = " + JSON.stringify(msg.mentions.users, null, 4));  // beautify
+    console.log("\n\n>>> sum = " + sum);
+    console.log("\n\n>>> msg.mentions.users <<<\n\n" + Mentioned);
+    console.log("\n\n>>> JSON.stringify(Mentioned) <<<\n\n" + MentionedSTR);  // print the object
+    console.log("\n\n>>> JSON.stringify(Mentioned, null, 4) <<<\n\n" + MentionedSTRbeauty);  // beautify
 } else {
     console.log("TIDAK ada mention");
+}
+
+// print the ID of each mentioned user
+console.log("\n\n>>> print the ID of each mentioned user <<<\n");
+for (i = 0; i < sum; i++) {
+    console.log("ID = " + JSON.parse(MentionedSTR)[i].id);
 }
 
 // referemce:
