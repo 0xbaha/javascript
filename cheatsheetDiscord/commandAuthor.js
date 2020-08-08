c7ca18ec058e117cc72f3d72405954e9d98459b4
Baha ID <dev@baha.web.id>
2020-08-08 23:16:33 +0700

    Update commandAuthor.js

--- cheatsheetDiscord/commandAuthor.js
+++ cheatsheetDiscord/commandAuthor.js
@@ -11,6 +11,8 @@ msg.author.createdAt        // account created date
 member.joinedAt             // joined date to server
 member.roles.cache.array()  // role at server
 
+user.presence.status        // presence status
+
 // Command Author Avatar URL
 msg.author.displayAvatarURL();
 msg.author.displayAvatarURL({ format: "png", dynamic: true, size: 2048 });
