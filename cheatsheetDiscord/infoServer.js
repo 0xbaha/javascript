cb0cacaae4b77e581356a3738bc61df5f17270c9
Baha ID <dev@baha.web.id>
2020-08-10 02:27:15 +0700

    Update infoServer.js

--- cheatsheetDiscord/infoServer.js
+++ cheatsheetDiscord/infoServer.js
@@ -1,24 +1,30 @@
-msg.guild.name                    // Server Name
-msg.guild.id                      // Server ID
-msg.guild.region                  // Server Region 
-msg.guild.createdAt               // Server Creation Date
-msg.guild.verificationLevel       // Server Verification Level
-msg.guild.explicitContentFilter   // Server Explicit Content Filter 
-msg.guild.owner.displayName       // Server Owner Display Name
-msg.guild.available               // Server Available Status
-msg.guild.memberCount             // Server Member Count
-msg.guild.emojis.cache.size       // Server Emojis Count
-msg.guild.roles.cache.size        // Server Count of Roles
-msg.guild.channels.cache.size     // Server Count of Channels
-msg.guild.afkChannel              // Server AFK Channel
-msg.guild.afkTimeout              // Server AFK Timeout (in seconds)
+console.log("Name           = " + msg.guild.name);
+console.log("ID             = " + msg.guild.id);
+console.log("Icon URL       = " + msg.guild.iconURL({ dynamic: true })); 
+console.log("Region         = " + msg.guild.region); 
+console.log("Creation Date  = " + msg.guild.createdAt); 
+console.log("Owner Name     = " + msg.guild.owner.displayName); 
+console.log("Avail Status   = " + msg.guild.available); 
+console.log("Member Count   = " + msg.guild.memberCount); 
+console.log("Emojis Count   = " + msg.guild.emojis.cache.size); 
+console.log("# of Roles     = " + msg.guild.roles.cache.size);  
+console.log("# of Channels  = " + msg.guild.channels.cache.size); 
 
-msg.guild.iconURL({ format: "png", dynamic: true, size: 2048 })                 // Server Icon
+console.log("Verification Level       = " + msg.guild.verificationLevel); 
+console.log("Explicit Content Filter  = " + msg.guild.explicitContentFilter); 
 
-msg.guild.emojis.cache.array()                                                  // Server Emojis List
-msg.guild.members.cache.filter(m => m.user.bot).size                            // Numbers of Bot Accounts
-msg.guild.members.cache.filter(m => !m.user.bot).size                           // Numbers of non-Bot Accounts
-msg.guild.members.cache.filter(m => m.user.presence.status === "online").size   // Server Online members
-msg.guild.members.cache.filter(m => m.user.presence.status === "offline").size  // Server Offline members
-msg.guild.members.cache.filter(m => m.user.presence.status === "idle").size     // Server DND members
-msg.guild.members.cache.filter(m => m.user.presence.status === "dnd").size      // Server Online members
+console.log("Count of Text Channels   = " + msg.guild.channels.cache.find(c => c.type === 'text').length);
+console.log("Count of Voice Channels  = " + msg.guild.channels.cache.find(c => c.type === 'voice').length);
+
+console.log("Online members = " + msg.guild.members.cache.filter(m => m.user.presence.status === "online").size); 
+console.log("Offine members = " + msg.guild.members.cache.filter(m => m.user.presence.status === "offline").size); 
+console.log("Idle members   = " + msg.guild.members.cache.filter(m => m.user.presence.status === "idle").size); 
+console.log("DND members    = " + msg.guild.members.cache.filter(m => m.user.presence.status === "dnd").size); 
+
+console.log("AFK Channel              = " + msg.guild.afkChannel);
+console.log("AFK Timeout (in seconds) = " + msg.guild.afkTimeout);  
+
+console.log("non-Bot Accounts  = " + msg.guild.members.cache.filter(m => !m.user.bot).size); 
+console.log("Bot Accounts      = " + msg.guild.members.cache.filter(m => m.user.bot).size); 
+
+msg.guild.emojis.cache.array();  //  Server Emojis List
